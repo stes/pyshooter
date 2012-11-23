cfa39160c47f78751b005360155570fa69c31516
Alex <alex.belke@gmail.com>
2012-11-23 18:57:31 +0100

    ...

--- core/entities.py
+++ core/entities.py
@@ -3,24 +3,32 @@ Created on 23.11.2012
 
 @author: Steffen
 '''
+import pygame, math, tools
 
 class Entity():
     
     def __init__(self, x, y, img):
         self.location = [x, y]
-        self.velocity = [0, 0]
+        self.angle = 0
+        self.velocity = 0
         self.img = img
+        self.bufimg = self.img
+        self.bufrect = self.bufimg.get_rect()
     
-    def accelerate(self, ddx, ddy):
-        self.velocity[0] += ddx
-        self.velocity[1] += ddy
-        
+    def accelerate(self, a):
+        self.velocity += a
+    
+    def rotate(self, rad):
+        self.angle += rad
+        self.bufimg, self.bufrect = tools.rot_center(self.img, self.img.get_rect(), self.angle * 180 / math.pi)
+    
     def move(self):
-        self.location[0] += self.velocity[0]
-        self.location[1] += self.velocity[1]
+        self.location[0] -= math.sin(self.angle) * self.velocity
+        self.location[1] -= math.cos(self.angle) * self.velocity
     
     def render(self, display):
-        display.blit(self.img, self.location)
+        rect = self.bufrect
+        display.blit(self.bufimg, [self.location[i] + rect[i] for i in [0, 1]])
 
 class Tank(Entity):
     
--- core/test.py
+++ core/test.py
@@ -1,16 +1,18 @@
 
-import sys, pygame, os
 from entities import *
+import os
+import pygame
+import sys
 
 pygame.init()
 
 size = width, height = 800, 600
 speed = [1, 1]
-black = 0, 0, 0
+white = 0xff, 0xff, 0xff
 
 screen = pygame.display.set_mode(size)
 
-ball = Entity(100, 100, pygame.image.load("tank1.gif"))
+tank = Entity(100, 100, pygame.image.load("tank1.gif").convert_alpha())
 
 while 1:
     for event in pygame.event.get():
@@ -18,15 +20,15 @@ while 1:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                ball.accelerate(-1, 0)
+                tank.rotate(+math.pi/16)
             elif event.key == pygame.K_RIGHT:
-                ball.accelerate(1, 0)
+                tank.rotate(-math.pi/16)
             elif event.key == pygame.K_UP:
-                ball.accelerate(0, -1)
+                tank.accelerate(0.5)
             elif event.key == pygame.K_DOWN:
-                ball.accelerate(0, 1)
-    ball.move()
-    screen.fill(black)
-    ball.render(screen)
+                tank.accelerate(-0.5)
+    tank.move()
+    screen.fill(white)
+    tank.render(screen)
     pygame.display.flip()
     pygame.time.wait(10)
