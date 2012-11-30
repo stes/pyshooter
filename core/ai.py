1beda95b7257e2648f8fa1d3cb12434b3e143e8d
stes <stes94@ymail.com>
2012-11-30 22:11:25 +0100

    added ai

---
+++ core/ai.py
@@ -0,0 +1,36 @@
+'''
+Created on 25.11.2012
+
+@author: Steffen
+'''
+
+import pygame
+from entities import Tank, Base, Entity
+import math
+
+last_error = 90
+actions = ["gun_right", "gun_left"]
+last_action = 0
+wait = 2
+
+def perform_action(tank, opponent, world, screen):
+    global last_error, actions, last_action, wait
+    [dx, dy] = [opponent.location[i] - tank.location[i] for i in [0,1]]
+
+    angle = -math.pi/2 - math.atan2(dy, dx)
+    g = tank.aim_direction
+    [bx, by] = [-math.sin(g), -math.cos(g)]
+    
+    [x, y] = tank.location
+    
+    pygame.draw.line(screen, (128, 0, 0), [x, y], [x+100*dx, y+100*dy])
+    pygame.draw.line(screen, (0, 128, 0), [x, y], [x+100*bx, y+100*by])
+        
+    error = math.acos((dx*bx + dy*by) / (math.hypot(dx, dy) * math.hypot(bx, by)))
+    
+    diff = last_error - error;
+    print diff
+    
+
+    tank.perform_action("gun_right", -diff*10)
+    last_error = error
