01c9fe172b5c9a53c4c3580e6f6e5cb9a7629146
Alex <alex.belke@gmail.com>
2013-01-26 20:53:11 +0100

    added exercise for main and entities

--- core/entities.py
+++ core/entities.py
@@ -14,69 +14,72 @@ game_map = pygame.Rect(0, 0, 800, 600)
 class Entity():
     
     def __init__(self, x, y, img, priority):
-        self.location = Vector2D(x, y)
-        self.angle = 0
-        self.velocity = 0
-        self.acceleration = 0
-        self.turn_velocity = 0
-        self.turn_acceleration = 0
+        #
+        # ADD CODE HERE
+        #
         self.img = img
         self.bufimg = self.img
         if self.bufimg != None: self.bufrect = self.bufimg.get_rect()
         self.map_rect = game_map
-        self.priority = priority
     
     def set_angle(self, angle):
         self.angle = angle
         if self.bufimg != None:
+            # rotate image
             self.bufimg, self.bufrect = tools.rot_center(self.img, self.img.get_rect(), self.angle * 180 / math.pi)
-    
-    def move(self):
-        self.location -= Vector2D(
-            math.sin(self.angle) * self.velocity,
-            math.cos(self.angle) * self.velocity)
-    
+            
     def render(self, display):
-        l = self.location.list()
-        display.blit(self.bufimg, [l[i] - self.bufrect[i+2]/2 for i in [0, 1]])
+        display.blit(self.bufimg, [self.location[i] - self.bufrect[i+2]/2 for i in [0, 1]])
     
-    def get_radius(self):
-        return (self.img.get_rect()[2] + self.img.get_rect()[3]) / 2
-    
-    def collide(self, vector):
-        return self.get_radius()**2 > (self.location-vector).square()
-    
-    def collide_entities(self, other):
-        return 0.3*(self.get_radius()+other.get_radius())**2 > (self.location-other.location).square()
-
     def alive(self):
         return True
+    
+    def tick(self):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def get_radius(self):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def collide(self, vector):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def collide_entities(self, other):
+        pass
+        #
+        # ADD CODE HERE
+        #
 
 class Tank(Entity):
-    MAX_SPEED = 3
-    MAX_TURN_SPEED = 0.01 * math.pi
+    # some constants
     MAX_AMMO = 10
     MAX_HEALTH = 100
     RELOAD_TIME = 100
-    DCL_AIM = 0.005 * math.pi
-    DCL_TURN = 0.0005 * math.pi
-    DCL_MOVE = 0.2
+    #
+    # Add some more constants!
+    #
+    # ADD CODE HERE
+    #
     
     def __init__(self, x, y, img, top_img, key_binding):
         Entity.__init__(self, x, y, img, 10)
-        self.aim_direction = 0
+        #
+        # ADD CODE HERE
+        #
         self.top_img = top_img
         self.top_bufimg = top_img
         if self.top_bufimg != None: self.top_bufrect = top_img.get_rect()
-        self.aim_velocity = 0
-        self.aim_acceleration = 0
         self.old_location = self.location
-        self.shoot_reload = 0
         self.key_binding = key_binding
-        self.ammo = Tank.MAX_AMMO
-        self.health = Tank.MAX_HEALTH
         self.last_action = [0, 0, 0]
-        self.base = None
     
     def set_base(self, base):
         self.base = base
@@ -99,83 +102,12 @@ class Tank(Entity):
         elif vel < 0:
             vel = min(vel + dec, 0)
         return vel
-            
-    def move(self):
-        # save copy of old location
-        self.old_location = self.location.copy()
-        
-        # update velocities
-        self.aim_velocity = self.decelerate(self.aim_acceleration, self.aim_velocity, Tank.DCL_AIM, Tank.MAX_TURN_SPEED)
-        self.turn_velocity = self.decelerate(self.turn_acceleration, self.turn_velocity, Tank.DCL_TURN, Tank.MAX_TURN_SPEED)
-        mult = 0.25 if self.velocity * self.acceleration > 0 else 1
-        self.velocity = self.decelerate(self.acceleration, self.velocity, Tank.DCL_MOVE, Tank.MAX_SPEED, mult)
-        
-        # update positions
-        self.rotate_gun_to(self.aim_direction + self.aim_velocity)
-        self.set_angle(self.angle + self.turn_velocity)
-        Entity.move(self)
-        
-        # check if position is valid
-        self.check_borders()        
-        
-        # reload the gun
-        self.shoot_reload = min(self.shoot_reload+1, Tank.RELOAD_TIME)
-        
-        # prevent to get killed
-        return self.alive()
-    
-    def move_copy(self, action):
-        tank = Tank(self.location[0], self.location[1], self.img, self.top_img, None)
-        tank.aim_direction = self.aim_direction
-        tank.aim_velocity = self.aim_velocity
-        tank.aim_acceleration = self.aim_acceleration
-        tank.old_location = self.old_location[:]
-        tank.shoot_reload = self.shoot_reload
-        tank.ammo = self.ammo
-        tank.health = self.health
-        tank.last_action = action
-        tank.perform_action(action)
-        tank.move()
-        return tank
-        
-    
-    def check_borders(self):
-        l = self.location
-        for i in [0, 1]:
-            if l[i]-self.bufrect[i+2]/2 < self.map_rect[i]:
-                l[i] = self.map_rect[i] + self.bufrect[i+2]/2
-            if l[i]+self.bufrect[i+2]/2 > self.map_rect[i]+self.map_rect[i+2]:
-                l[i] = self.map_rect[i]+self.map_rect[i+2] - self.bufrect[i+2]/2
-    
-    def rotate_gun_to(self, angle):
-        self.aim_direction = angle % (math.pi * 2)
-        if self.top_bufimg != None:
-            self.top_bufimg, self.top_bufrect = tools.rot_center(self.top_img, self.top_img.get_rect(), self.aim_direction * 180 / math.pi)
     
     def render(self, display):
         Entity.render(self, display)
         rect = self.top_bufrect
         display.blit(self.top_bufimg, [self.location.list()[i] - rect[i+2]/2 for i in [0, 1]])
-    
-    def damage(self, missile):
-        if missile.owner != self:
-            self.health -= missile.damage
-            missile.destroy()
-    
-    def alive(self):
-        return self.health > 0
-
-    def shoot(self):
-        if self.ready_to_shoot():
-            safe_dist = Vector2D(math.sin(self.aim_direction) * 40,
-                                 math.cos(self.aim_direction) * 40)
-            
-            start_pos = self.location - safe_dist
-            missile = Missile(start_pos.x, start_pos.y, self)
-            self.ammo -= 1
-            self.shoot_reload = 0
-            self.world.append([missile.priority, missile])
-    
+        
     def set_world(self, world):
         self.world = world
     
@@ -195,67 +127,98 @@ class Tank(Entity):
                             int("gun_fire" in actions)]
         self.perform_action(self.last_action)
     
-    def perform_action(self, actions):
-        move = actions[0] - actions[1]
-        turn = actions[2] - actions[3]
-        aim = actions[4] - actions[5]
-        self.last_action = actions[:]
-        self.perform_action_move(move, turn, aim)
-        if actions[6]: self.shoot()
-    
     def perform_action_move(self, move=0, turn=0, aim=0):
         self.acceleration = int(move)
         self.turn_acceleration = int(turn)
         self.aim_acceleration = int(aim)
+        
+    def shoot(self):
+        if self.ready_to_shoot():
+            safe_dist = Vector2D(math.sin(self.aim_direction) * 40,
+                                 math.cos(self.aim_direction) * 40)
+            
+            start_pos = self.location - safe_dist
+            #
+            # ADD CODE HERE
+            #
     
-    def get_repr(self):
-        return [self.location[0] / self.map_rect.width,
-                self.location[1] / self.map_rect.height,
-                self.angle / (math.pi * 2.),
-                self.velocity / Tank.MAX_SPEED,
-                self.aim_direction / (math.pi * 2),
-                self.aim_velocity / Tank.MAX_TURN_SPEED,
-                self.turn_velocity / Tank.MAX_TURN_SPEED,
-                self.ammo / float(Tank.MAX_AMMO),
-                self.health / float(Tank.MAX_HEALTH)]
+    def tick(self):
+        # save copy of old location
+        self.old_location = self.location.copy()
+        
+        # update velocities
+        #
+        # ADD CODE HERE
+        #
+        
+        # update positions
+        #
+        # ADD CODE HERE
+        #
+        
+        # check if position is valid
+        #
+        # ADD CODE HERE
+        #       
+        
+        # reload the gun
+        #
+        # ADD CODE HERE
+        #        
+    
+    def check_borders(self):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def rotate_gun_to(self, angle):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def damage(self, missile):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def alive(self):
+        pass
+        #
+        # ADD CODE HERE
+        #
+    
+    def perform_action(self, actions):
+        pass
+        #
+        # ADD CODE HERE
+        #
                 
                     
 class Missile(Entity):
-    missile_img = None
     
-    def __init__(self, x, y, owner):
-        Entity.__init__(self, x, y, Missile.missile_img, 5)
-        self.velocity = 10
-        self.owner = owner
-        self.set_angle(owner.aim_direction)
-        self.ttl = 200
-        self.damage = 10
+    def __init__(self, x, y, img, owner):
+        pass
+        #
+        # ADD CODE HERE
+        #
     
-    def destroy(self):
-        self.ttl = 0
-    
-    def alive(self):
-        return (self.ttl > 0) and (self.velocity > 2)
-    
-    def move(self):
-        Entity.move(self)
-        self.ttl -= 1
-        if self.location.x < 0 or self.location.x > self.map_rect.width or \
-            self.location.y < 0 or self.location.y > self.map_rect.height:
-            self.destroy()
+    # Implement the methods "destroy", "alive" and "move"
+    #
+    # ADD CODE HERE
+    #
+
 
 class Base(Entity):
     
-    def __init__(self, x, y, img, owner):
-        Entity.__init__(self, x, y, img, 0)
-        self.owner = owner
-        self.preparation_time = 0
-        self.owner.set_base(self)
+    # implement the constructor!
+    #
+    # ADD CODE HERE
+    #
     
-    def get_radius(self):
-        return 10
-    
-    def move(self):
+    def tick(self):
         if self.owner.collide_entities(self) and self.preparation_time == 0:
             while (self.owner.ammo < 10):
                 self.preparation_time += 50
--- core/main.py
+++ core/main.py
@@ -7,10 +7,6 @@ handles the interaction between players in the game.
 
 
 from entities import Tank, Missile, Base
-from logger import Logger
-from particlesys import ParticleSystem
-import ai
-import copy
 import pygame
 import sys
 
@@ -21,8 +17,6 @@ WIDTH = 800
 HEIGHT = 600
 '''                    '''
 
-psys = ParticleSystem()
-
 ''' Key bindings for both players '''
 KEY_BINDINGS_1 = {pygame.K_LEFT: "left",
                 pygame.K_RIGHT: "right",
@@ -31,13 +25,11 @@ KEY_BINDINGS_1 = {pygame.K_LEFT: "left",
                 pygame.K_n: "gun_left",
                 pygame.K_COMMA: "gun_right",
                 pygame.K_m: "gun_fire"}
-KEY_BINDINGS_2 = {pygame.K_a: "left",
-                pygame.K_d: "right",
-                pygame.K_w: "up",
-                pygame.K_s: "down",
-                pygame.K_g: "gun_left",
-                pygame.K_j: "gun_right",
-                pygame.K_h: "gun_fire"}
+#
+# Add key bindings for the second player
+#
+# ADD CODE HERE
+#
 '''                                '''
 
 input_listener = []
@@ -46,39 +38,13 @@ keys_pressed = []
 def check_collisions(world, entity):
     for p, e1 in world:
         if e1 != entity and e1.collide_entities(entity):
-            if isinstance(e1, Tank):
-                if isinstance(entity, Missile):
-                    e1.damage(entity)
-                    psys.explosion(entity.location.x, entity.location.y, 200)
-                if isinstance(entity, Tank):
-                    entity.step_back()
-                    e1.step_back()
-
-
-def render_gui(screen):
-    pygame.draw.rect(screen, pygame.Color(0x44, 0x44, 0x44), (0, 0, 500, 60))
-    pygame.draw.rect(screen, pygame.Color(0x44, 0x44, 0x44), (300, 540, 500, 60))
-    
-    missile_rect = missile_img.get_rect()
-    for i in range(tank2.ammo):
-        screen.blit(missile_img, [480-missile_rect[2]*i*2, 20])
-    
-    for i in range(tank1.ammo):
-        screen.blit(missile_img, [WIDTH - missile_rect[2]*i*2, HEIGHT-40])
-    
-    pygame.draw.rect(screen, pygame.Color(128, 0, 0), (10, 25, 200, 20), 2)
-    pygame.draw.rect(screen, pygame.Color(128, 0, 0), (10, 25, tank2.health*200/Tank.MAX_HEALTH, 20))
-    
-    pygame.draw.rect(screen, pygame.Color(0, 128, 0), (10, 10, 200, 10), 2)
-    pygame.draw.rect(screen, pygame.Color(0, 128, 0), (10, 10, tank2.shoot_reload*200/Tank.RELOAD_TIME, 10))
-
-    
-    pygame.draw.rect(screen, pygame.Color(128, 0, 0), (310, 565, 200, 20), 2)
-    pygame.draw.rect(screen, pygame.Color(128, 0, 0), (310, 565, tank1.health*200/Tank.MAX_HEALTH, 20))
-    
-    pygame.draw.rect(screen, pygame.Color(0, 128, 0), (310, 550, 200, 10), 2)
-    pygame.draw.rect(screen, pygame.Color(0, 128, 0), (310, 550, tank1.shoot_reload*200/Tank.RELOAD_TIME, 10))
-
+            pass
+            # Tank:
+            # check for collision with:
+            # other tank, missile (-> explosion)
+            #
+            # ADD CODE HERE
+            #
 
 def game_loop(tank1, tank2, world):
     '''
@@ -96,59 +62,43 @@ def game_loop(tank1, tank2, world):
     the order specified above
     (iv) Finally, we wait some time before performing the next loop
     '''
-    logger = Logger('locs.dat')
     
     clock = pygame.time.Clock()
     
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                del(logger)
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 keys_pressed.append(event.key)
             elif event.type == pygame.KEYUP:
                 keys_pressed.remove(event.key)
+                
+        # Broadcast events
         for l in input_listener:
             l.on_input(keys_pressed)
 
         screen.fill(pygame.Color(255, 255, 255)) # white
-        screen.blit(background_img, background_img.get_rect())
+        # render background image
+        #
+        # ADD CODE HERE
+        #
+        
+        # Sort world by priority
         world.sort()
         
-        world_copy = [copy.copy(world[i]) for i in range(len(world))]
-        tank1_copy = copy.deepcopy(tank1)
-        tank2_copy = copy.deepcopy(tank2)
-#        ai.observe(tank1, tank2_copy, world_copy, screen)
-#        ai.observe(tank2, tank1_copy, world_copy, screen)
-        #ai.perform_action(tank1, tank2_copy, world_copy)
-        tank1.action(tank2_copy, world_copy)
-        #tank2.action(tank1_copy, world_copy)
+        # entities:
+        # update, remove "dead" ones, check for collision, render
+        #
+        # ADD CODE HERE
+        #
         
-        for [p, entity] in world:
-            entity.move()
-            if not entity.alive():
-                world.remove([entity.priority, entity])
-            else:
-                check_collisions(world, entity)
-                entity.render(screen)
-        
-        logger.log(tank1, tank2, world)
-        
-        psys.tick()
-        psys.render(screen)
-        render_gui(screen)
-        
+        # Adjust framerate
         pygame.display.set_caption("FPS: %f" % (clock.get_fps(), ))
         pygame.display.flip()
         
         clock.tick(60)
 
-
-pygame.init()
-
-size = WIDTH, HEIGHT
-screen = pygame.display.set_mode(size)
 
 if __name__ == '__main__':
     '''
@@ -159,24 +109,26 @@ if __name__ == '__main__':
     All entities are placed in a list, which we call world.
     At last, the game loop can be started using this list.
     '''
-
-    tank1 = ai.AIBot(100, 500, pygame.image.load("tank1.gif"), pygame.image.load("tank1_top.gif"), KEY_BINDINGS_1)
-    tank2 = Tank(700, 100, pygame.image.load("tank2.gif"), pygame.image.load("tank2_top.gif"), KEY_BINDINGS_2)
     
-    #input_listener.append(tank1)
-    input_listener.append(tank2)
+    # Initialize pygame and get a surface to draw on
+    pygame.init()
+    size = WIDTH, HEIGHT
+    screen = pygame.display.set_mode(size)
     
-    base1 = Base(100, 500, pygame.image.load("base.gif"), tank1)
-    base2 = Base(700, 100, pygame.image.load("base.gif"), tank2)
     
-    background_img = pygame.image.load('dirt.jpg')
-    background_img = pygame.transform.scale(background_img, size)
+    # load the resources
+    # initialize the basic entities (base, tank)
+    #
+    # ADD CODE HERE
+    #
     
-    missile_img = pygame.image.load("missile.gif")
-    Missile.missile_img = missile_img
+    # Add tanks to input listeners
+    #
+    # ADD CODE HERE
+    #
     
-    world = [base1, base2, tank1, tank2]
-    world = [[e.priority, e] for e in world]
-    tank1.set_world(world)
-    tank2.set_world(world)
-    game_loop(tank1, tank2, world)
+    # Add both tanks to the world and start
+    # the game loop
+    #
+    # ADD CODE HERE
+    #
