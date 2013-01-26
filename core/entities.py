00f10a638a06d86b4326dc831352ea3d7e15ce7d
Alex <alex.belke@gmail.com>
2013-01-26 18:47:25 +0100

    --

--- core/entities.py
+++ core/entities.py
@@ -221,7 +221,7 @@ class Tank(Entity):
                 
                     
 class Missile(Entity):
-    missile_img = 0
+    missile_img = None
     
     def __init__(self, x, y, owner):
         Entity.__init__(self, x, y, Missile.missile_img, 5)
